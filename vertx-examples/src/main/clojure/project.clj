1f7750ddf506bd87170aacc2b2adbc187b5e514f
Isaiah Peng <isaiah.peng@vcint.com>
2012-12-22 00:42:12 +0100

    add lein-vertx plugin

--- vertx-examples/src/main/clojure/project.clj
+++ vertx-examples/src/main/clojure/project.clj
@@ -8,6 +8,6 @@
                  [org.vert-x/vertx-platform "1.3.0.final"]
                  [org.clojure/core.match "0.2.0-alpha11"]
                  [org.clojure/tools.logging "0.2.3"]]
- ; :plugins [[lein-vertx "0.1.0-SNAPSHOT"]]
+  :plugins [[lein-vertx "0.1.0-SNAPSHOT"]]
   )
 
